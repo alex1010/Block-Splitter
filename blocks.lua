97b16237e48a1c1d7ca67bd231970d0ec1cb0c5a
alex1010 <doloz.alexander@gmail.com>
2015-06-27 07:03:14 +0600

    Setup blocks.lua

---
+++ blocks.lua
@@ -0,0 +1,19 @@
+local blocks = {}
+
+function blocks:setup(setupTable)
+
+end
+
+function blocks:split(point)
+
+end
+
+function blocks:merge(point, direction)
+
+end
+
+function blocks:swap(point, direction)
+
+end
+
+return blocks
