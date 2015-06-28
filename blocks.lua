08f87e77468e1c5b83550d92ac894ea66c973758
alex1010 <doloz.alexander@gmail.com>
2015-06-28 13:57:34 +0600

    Implement setup method

--- blocks.lua
+++ blocks.lua
@@ -1,14 +1,21 @@
 local blocks = {}
+local ColoredRect = require "ColoredRect"
+local Rect = require "Rect"
+local Point = require "Vector2d"
 
 function blocks:setup(setupTable)
 	self.blocks = {}
 	for i = 1, #setupTable do 
 		local setupItem = setupTable[i]
+		local rect = Rect:new(Point.new(setupItem[1], setupItem[2]), Point.new(setupItem[3], setupItem[4]))
+		local color = setupItem[5]
+		local coloredRect = ColoredRect:new(rect, color)
+		self.blocks[i] = coloredRect
 	end
 end
 
 function blocks:split(point)
-
+	
 end
 
 function blocks:merge(point, direction)
